43efe9d10a383a0cd0399c8b044e9f5e93d11522
maykonestacio <maykon.estacio29@gmail.com>
2025-01-21 17:26:49 +0000

    WIP

---
+++ build/src/public/js/class/script.js
@@ -0,0 +1,46 @@
+"use strict";
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const container = document.querySelector(".container");
+function toggleMode() {
+    container.classList.toggle("dark-mode");
+}
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You most write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.classList.add("content");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+inputBox.addEventListener("keypress", function (e) {
+    if (e.key == "Enter") {
+        e.preventDefault();
+        document.querySelector("#button-input-box").click();
+    }
+});
